9e8b51cc2eb3583211cb4622007001fdaa2d9b9d
ZhukovaSveta <thelumierlana@yandex.ru>
2022-05-19 23:14:20 +0300

    add button

---
+++ 2.0/hwVerstka 2.0/hwVerstka 2.0/hwVerstka_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  hwVerstka_2_0App.swift
+//  hwVerstka 2.0
+//
+//  Created by Надежда Жукова on 19.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct hwVerstka_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- verstka/verstka/ViewController.swift
+++ verstka/verstka/ViewController.swift
@@ -1,33 +1,4 @@
-//
-//  ViewController.swift
-//  verstka
-//
-//  Created by Надежда Жукова on 18.05.2022.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-    
-    private var label: UILabel {
-        let label = UILabel()
-        label.text = "Имя Фамилия"
-        return label
-    }
-    
-    
-    private lazy var textName = UITextField()
-    private lazy var button = UIButton()
-
-
-    override func viewDidLoad() {
-        setupView()
-        super.viewDidLoad()
-    }
-
-    private func setupView() {
-        view.backgroundColor = .systemGray6
-    }
-
-}
 
